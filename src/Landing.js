84c324edc290562a7b1713f849b9144ecd86639b
jesChung <gracefuleverymoment@gmail.com>
2022-05-18 13:34:29 +0800

    combine into landing page

---
+++ src/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header'
+import Content from './components/Content'
+
+function Home() {
+  return (
+    <div>
+        <Header />
+        <Content />
+    </div>
+  )
+}
+
+export default Home
